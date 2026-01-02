f0f58db714ce513253e915c67638986df4132263
Virgile AE <phaeridefmatel@outlook.com>
2026-01-02 22:43:58 +0100

    update

---
+++ ai/__init__.py
@@ -0,0 +1,8 @@
+from utils import system_checks
+# -*- coding: utf-8 -*-
+"""AI module for neural network processing"""
+
+from .neural_engine import NeuralEngine
+from .model_loader import ModelLoader
+
+__all__ = ['NeuralEngine', 'ModelLoader']
